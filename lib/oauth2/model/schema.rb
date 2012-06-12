e1795f0ddb927a5bed8dc7fe177f038f40ecef0a
Justin Weiss <justin@uberweiss.org>
2012-06-12 15:56:55 -0700

    Un-hashify the access and refresh tokens.
    Currently, logging in with separate instances of the same client will
    force every instance except one to re-login. This isn't the behavior
    we want. http://stackoverflow.com/questions/5022471/best-practices-for-handling-access-tokens-and-scopes-for-oauth2-implementation
    has convinced me that the 'one shared token per client per owner' is
    the best behavior for our purposes.

--- lib/oauth2/model/schema.rb
+++ lib/oauth2/model/schema.rb
@@ -1,6 +1,6 @@
 module OAuth2
   module Model
-    
+
     class Schema < ActiveRecord::Migration
       def self.up
         create_table :oauth2_clients, :force => true do |t|
@@ -13,7 +13,7 @@ module OAuth2
           t.string     :redirect_uri
         end
         add_index :oauth2_clients, :client_id
-        
+
         create_table :oauth2_authorizations, :force => true do |t|
           t.timestamps
           t.string     :oauth2_resource_owner_type
@@ -21,22 +21,21 @@ module OAuth2
           t.belongs_to :client
           t.string     :scope
           t.string     :code,               :limit => 40
-          t.string     :access_token_hash,  :limit => 40
-          t.string     :refresh_token_hash, :limit => 40
+          t.string     :access_token,       :limit => 40
+          t.string     :refresh_token,      :limit => 40
           t.datetime   :expires_at
         end
         add_index :oauth2_authorizations, [:client_id, :code]
-        add_index :oauth2_authorizations, [:access_token_hash]
-        add_index :oauth2_authorizations, [:client_id, :access_token_hash]
-        add_index :oauth2_authorizations, [:client_id, :refresh_token_hash]
+        add_index :oauth2_authorizations, [:access_token]
+        add_index :oauth2_authorizations, [:client_id, :access_token]
+        add_index :oauth2_authorizations, [:client_id, :refresh_token]
       end
-      
+
       def self.down
         drop_table :oauth2_clients
         drop_table :oauth2_authorizations
       end
     end
-    
+
   end
 end
-
